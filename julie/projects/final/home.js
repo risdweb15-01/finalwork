5cb0067bfb93d91328d5a8dd246284df3d349c13
jrhee1031 <jrhee@risd.edu>
2015-02-02 13:32:49 -0500

    commut

--- julie/projects/final/home.js
+++ julie/projects/final/home.js
@@ -1,10 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-    $('.checkboxList .row').css('color','red');
-    $('.checkboxList input').attr('checked', true);
-    $('.checkboxList input').bind('click', function() {
-    	$('#message').html("You clicked on a checkbox.").fadeIn('slow');
-    });
 
-	});
+
+$( "#enter" ).click(function() {
+  $( ".transition" ).fadeOut( "slow", function() {
+    // Animation complete
+  });
+});
+
+
+
+
+
+
+});
