c16e9c25d1b990a4a7f0be44c494de844e28f880
mcaicedo2015 <user@mbp-antonia.risd.edu>
2015-02-03 13:54:11 -0500

    EDIT
    EDIT

--- maria/Projects/project3/js/script.js
+++ maria/Projects/project3/js/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-     
- $('.about-box').animatedBG({
-			colorSet: ['#04b8a0', '#f2004d', '#f15b22', '#02cd67'],
-			speed: 2000
-		});
-
-		
+	$('.minibiber').click(function(){
+		$(this).fadeIn('slow');
 
 	});
+});
+     
+ 
