a656b53d4f19288c2d743a51fcc6d3dc050cdb08
kwalters87 <kelly@brightpolkadot.com>
2015-02-03 14:23:10 -0500

    esther
    ester files

---
+++ esther/projects/Project 3/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+		$('.b1').fadeIn('slow');
+		$('.b2').fadeIn('slow');
+		$('.b3').fadeIn('slow');
+		$('.b4').fadeIn('slow');
+		$('.b5').fadeIn('slow');
+		$('.b6').fadeIn('slow');
+		$('.y2').fadeIn('slow');
+});
